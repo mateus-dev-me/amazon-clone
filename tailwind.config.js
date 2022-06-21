6f8d4ef00306b77d68fe88941ae86864486a0867
Mateus-Sousa23 <mateusmartinsipu2@gmail.com>
2022-06-21 17:36:14 -0300

    [add]: tailwind confing

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        arial: "Arial",
+      },
+      colors: {
+        "darkblue-500": "#485769",
+        "darkblue-600": "#37475A",
+        "darkblue-700": "#232F3E",
+        "darkblue-900": "#191E26",
+        "darkblue-950": "#131A22",
+        "gold-100": "#FEBD69",
+        "gold-200": "#FAA63A",
+        "orange-650": "#E47911",
+        "yellow-350": "#F0C14B",
+      },
+      spacing: {
+        7: "1.75rem",
+        "75px": "75px",
+        "210px": "210px",
+      },
+    },
+  },
+  plugins: [],
+};
